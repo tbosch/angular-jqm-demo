0ef964520b08878aa449249aa1fbd238fcb08f48
Tobias Bosch <tbosch1009@gmail.com>
2013-10-23 14:10:46 -0700

    initial commit

---
+++ app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('angularMobileDemoApp', ['jqm'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/page1', {
+        templateUrl: 'views/page1/page1.html',
+        controller: 'Page1Ctrl',
+        animation: 'page-slide'
+      })
+      .when('/page2', {
+        templateUrl: 'views/page2/page2.html',
+        controller: 'Page2Ctrl',
+        animation: 'page-slide'
+      })
+      .otherwise({
+        redirectTo: '/page1'
+      });
+  });
